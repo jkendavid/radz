ee0eb5a8d8209d0fed686a6c7fadf65648996f8e
jkendavid <108902987+jkendavid@users.noreply.github.com>
2022-07-08 06:44:48 +0800

    first commit

---
+++ LifeGoals.js
@@ -0,0 +1,132 @@
+﻿$('.numberonly').on('input', function () {
+    var c = this.selectionStart,
+        r = /[^0-9]/gi,
+        v = $(this).val();
+    if (r.test(v)) {
+        $(this).val(v.replace(r, ''));
+        c--;
+    }
+    this.setSelectionRange(c, c);
+})
+
+$("#inputMonthlyExpeses").on('keyup', function () {
+    $('#expeses5years').val(($(this).val() * 60).toLocaleString())
+});
+
+
+//add college schools
+var inputEducSchool = $('#inputEducSchool')
+$(tuitionFeeList.List).each(function (i, v) {
+    inputEducSchool.append(`<option value="${v.Fee}">${v.College}</option>`)
+});
+inputEducSchool.append(`<option value="Others">Others</option>`)
+
+$('.updateEstimatedTuitionFee').on("change", function () {
+    updateEstimatedTuitionFee();
+})
+
+var updateEstimatedTuitionFee = function () {
+    var birthyear = parseInt($('#inputChildBirthYear').val())
+    var school = inputEducSchool.find('option:selected').text()
+
+    if (school == 'Others') {
+        $('#inputYearlyTuitionFee').attr('readonly', false);
+        $('#inputYearlyTuitionFee').val('')
+    }
+    else if (birthyear != 0 && school != '') {
+        $('#inputYearlyTuitionFee').attr('readonly', true);
+        var feebaseyear = dataInt(tuitionFeeList.Year)
+        var feeannualincrease = dataInt(tuitionFeeList.AnnualIncrease)
+
+        var startschoolyear = dataInt(birthyear + 18)
+        var fee = dataInt(inputEducSchool.val())
+
+        var projectedfee = dataInt(fee * (1 + (startschoolyear - feebaseyear) * feeannualincrease))
+        $('#inputYearlyTuitionFee').val(projectedfee.toLocaleString())
+    }
+}
+
+
+var updateEducChildTotal = function () {
+    var tbl = $('#tblEduc')
+    var totalEducFunding = 0
+    $(tbl.find('tbody tr')).each(function (i, v) {
+        totalEducFunding += dataInt($($(v).find('.educchildtotal')[0]).text())       
+    });
+    var tfooter = tbl.find('tfoot')
+    if (totalEducFunding > 0) {
+        tfooter.html(`<td></td><td></td><td></td><td></td><td></td><th>Total</th><th>₱${totalEducFunding.toLocaleString()}</th>`)
+    } else {
+        tfooter.html('')
+    }
+}
+
+
+var removEduc = function (c) {
+    c.closest('tr').remove()
+    updateEducChildTotal()
+}
+
+function dataInt(v) {
+    return v == '' ? 0 : parseInt(String(v).replace(/,/g, ""))
+}
+
+$("#inputEducAdd").on("click", function () {
+    var birthyear = parseInt($('#inputChildBirthYear').val())
+    var name = $('#inputEducName').val()
+    var school = inputEducSchool.find('option:selected').text()
+    if (name != '' && birthyear != 0 && school != '') {
+
+        var projectedfee = dataInt($('#inputYearlyTuitionFee').val())
+        var projectedallowance = dataInt($('#inputSchoolAllowance').val())
+        var projectedfeetotal = dataInt((projectedfee + (projectedallowance * 209)) * 4 )
+
+        var delbtn = `<button class="btn btn-danger inputEducDelete btn-sm" type="button" onclick="removEduc(this)">x</button>`
+        var tblbody = $('#tblEduc tbody')
+        tblbody.append(`<tr><td>${delbtn}</td><td>${name}</td><td>${birthyear}</td><td>${school}</td><td>${projectedfee.toLocaleString()}</td><td>${projectedallowance.toLocaleString() }</td><td class="educchildtotal">${projectedfeetotal.toLocaleString()}</td></tr>`);
+        $('#inputEducName').val('')
+        $('#inputChildBirthYear').val('')
+        inputEducSchool.val('')
+        $('#inputYearlyTuitionFee').val('')
+        $('#inputYearlySchoolAllowance').val('')
+        $('#inputEducFormAdd').modal('hide')
+        $('#inputYearlyTuitionFee').attr('readonly', true);
+        updateEducChildTotal()
+    } else {
+        alert("Name, Year before college and name of school are required!")
+    }
+});
+
+
+
+$("#dateOfBirth").blur(function () {
+    var birthyear = parseInt($(this).val().split("-")[0])
+    var currentYear = parseInt(new Date().getFullYear())
+    var age = currentYear - birthyear
+    $('#outputCurrentAge').val(age)
+    updateYearsBeforeRetirement()
+})
+
+$("#inputRetirementAge").on("change", function () {
+    updateYearsBeforeRetirement()
+})
+
+var updateYearsBeforeRetirement = function () {
+    var age = parseInt($('#outputCurrentAge').val())
+    var retirementAge = parseInt($('#inputRetirementAge').val())
+    $('#outputYearRetirement').val(retirementAge - age)
+}
+
+
+$(".updateTotalRetirementFund").on("change", function () {
+    updateTotalRetirementFund()
+})
+
+
+var updateTotalRetirementFund = function () {
+    var monthlyIncome = parseInt($('#inputMonthlyIncome').val())
+    var monthlyIncomePercentNeeded = parseInt($('#inputMonthlyIncomePercent').val())
+    var retirementYears = parseInt($('#inputRetirementYearsNeeded').val())
+    $('#outputTotalRetirementFund').val((monthlyIncome * monthlyIncomePercentNeeded * 12 * retirementYears / 100).toLocaleString())
+}
+
---
+++ TuitionFee.js
@@ -0,0 +1,188 @@
+var tuitionFeeList = {
+    "Year": 2023,
+    "AnnualIncrease":0.076,
+    "List": [
+        {
+            "College": "Enderun Colleges",
+            "Fee": 512086
+        },
+        {
+            "College": "University of Asia and the Pacific",
+            "Fee": 470760
+        },
+        {
+            "College": "De La Salle University Manila",
+            "Fee": 389904
+        },
+        {
+            "College": "De La Salle-College of Saint Benilde",
+            "Fee": 377327
+        },
+        {
+            "College": "Ateneo de Manila",
+            "Fee": 323423
+        },
+        {
+            "College": "San Beda College",
+            "Fee": 269519
+        },
+        {
+            "College": "Assumption College",
+            "Fee": 260535
+        },
+        {
+            "College": "Asia Pacific College",
+            "Fee": 255145
+        },
+        {
+            "College": "Miriam College",
+            "Fee": 251551
+        },
+        {
+            "College": "Mapua Institute of Technology",
+            "Fee": 242567
+        },
+        {
+            "College": "Far Eastern University",
+            "Fee": 206631
+        },
+        {
+            "College": "St. Scholastica's College",
+            "Fee": 197647
+        },
+        {
+            "College": "St. Paul University",
+            "Fee": 188663
+        },
+        {
+            "College": "Philippine Women's University",
+            "Fee": 186867
+        },
+        {
+            "College": "University of Santo Tomas",
+            "Fee": 179679
+        },
+        {
+            "College": "Don Bosco Technical College",
+            "Fee": 176086
+        },
+        {
+            "College": "San Sebastian College",
+            "Fee": 167102
+        },
+        {
+            "College": "Adamson University",
+            "Fee": 161711
+        },
+        {
+            "College": "AMA Computer University",
+            "Fee": 147337
+        },
+        {
+            "College": "University of the East",
+            "Fee": 145540
+        },
+        {
+            "College": "Centro Escolar University",
+            "Fee": 143743
+        },
+        {
+            "College": "Lyceum of the Philippines",
+            "Fee": 143743
+        },
+        {
+            "College": "College of the Holy Spirit",
+            "Fee": 143743
+        },
+        {
+            "College": "Colegio de San Juan de Letran",
+            "Fee": 140150
+        },
+        {
+            "College": "Trinity University of Asia",
+            "Fee": 127572
+        },
+        {
+            "College": "University of the Philippines",
+            "Fee": 106011
+        },
+        {
+            "College": "National University",
+            "Fee": 104214
+        },
+        {
+            "College": "University of Perpetual Help System",
+            "Fee": 102417
+        },
+        {
+            "College": "De La Salle University Dasmarinas",
+            "Fee": 215615
+        },
+        {
+            "College": "Holy Angels University",
+            "Fee": 116792
+        },
+        {
+            "College": "University of the Assumption",
+            "Fee": 107808
+        },
+        {
+            "College": "Ateneo de Naga",
+            "Fee": 107808
+        },
+        {
+            "College": "Our Lady of Fatima University",
+            "Fee": 98824
+        },
+        {
+            "College": "Saint Louis University",
+            "Fee": 89840
+        },
+        {
+            "College": "Cebu Doctors University",
+            "Fee": 149134
+        },
+        {
+            "College": "University of San Jose Recoletos",
+            "Fee": 132963
+        },
+        {
+            "College": "Siliman University",
+            "Fee": 123979
+        },
+        {
+            "College": "University of San Carlos",
+            "Fee": 116792
+        },
+        {
+            "College": "University of St. La Salle",
+            "Fee": 113198
+        },
+        {
+            "College": "La Consolacion College Bacolod",
+            "Fee": 100620
+        },
+        {
+            "College": "Ateneo de Davao University",
+            "Fee": 165305
+        },
+        {
+            "College": "Xavier University",
+            "Fee": 98824
+        },
+        {
+            "College": "Davao Doctors College",
+            "Fee": 97027
+        },
+        {
+            "College": "Liceo de Cagayan University",
+            "Fee": 80856
+        },
+        {
+            "College": "Capitol University",
+            "Fee": 80856
+        }
+    ]
+
+
+    }
